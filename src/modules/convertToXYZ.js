edcd0ce2ad0c55e2338a958231928b81c3d84a80
Viihna <viihna@voidfucker.com>
2024-07-11 05:13:40 -0400

    Update convertToXYZ.js

---
+++ src/modules/convertToXYZ.js
@@ -0,0 +1,8 @@
+// Color Palette Generator - version 0.31
+// Licensed under GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.html)
+// Author: Viihna Lehraine (reach me at viihna@voidfucker.com / viihna.78 (Signal) / Lost-Possum (Github))
+
+// BEGIN CODE
+
+
+//
