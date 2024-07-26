741267f034b05c4a9b6e8f714e98d851c70c55f2
Viihna <viihna@voidfucker.com>
2024-07-26 09:01:54 -0400

    mobile edit commit

--- src/initHelpers.js
+++ src/initHelpers.js
@@ -1,4 +1,5 @@
-const generateButton = document.getElementById('generate-button');
+function initializeButtons() {
+    const generateButton = document.getElementById('generate-button');
     const saturateButton = document.getElementById('saturate-button');
     const desaturateButton = document.getElementById('desaturate-button');
     const popupDivButton = document.getElementById('custom-color-button');
@@ -8,3 +9,4 @@ const generateButton = document.getElementById('generate-button');
     const applyInitialColorSpaceButton = document.getElementById('apply-initial-color-space-button');
     let selectedColorOptions = document.getElementById('selected-color-options');
     let selectedColor = parseInt(selectedColorOptions.value, 10);
+};
