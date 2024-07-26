dab735fb5e38e33f5cd7d6f379dd589adb3da375
Viihna <viihna@voidfucker.com>
2024-07-26 09:00:07 -0400

    created initHelpers.js (from mobile)

---
+++ src/initHelpers.js
@@ -0,0 +1,10 @@
+const generateButton = document.getElementById('generate-button');
+    const saturateButton = document.getElementById('saturate-button');
+    const desaturateButton = document.getElementById('desaturate-button');
+    const popupDivButton = document.getElementById('custom-color-button');
+    const applyColorButton = document.getElementById('apply-color-button');
+    const clearColorButton = document.getElementById('clear-color-button');
+    const advancedMenuToggleButton = document.getElementById('advanced-menu-toggle-button');
+    const applyInitialColorSpaceButton = document.getElementById('apply-initial-color-space-button');
+    let selectedColorOptions = document.getElementById('selected-color-options');
+    let selectedColor = parseInt(selectedColorOptions.value, 10);
